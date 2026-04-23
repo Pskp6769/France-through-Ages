8866e12f07c84c9257a4927cd576b50b5f476b77
Pskp6769 <kuttipskp@gmail.com>
2026-04-23 20:40:33 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
-
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+ 
+export default defineConfig({
+  base: '/France-through-Ages/',
+  plugins: [react()],
   server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
+    port: 5173,
+    open: true,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-    dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime", "@tanstack/react-query", "@tanstack/query-core"],
-  },
-}));
+})
+ 
